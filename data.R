c92b2fea11228474b9c7b879b70cf471c4b2d668
Thanh An <lvuthanhan1107@gmail.com>
2021-11-24 19:59:46 +0700

    An update 24/11/2021

--- data.R
+++ data.R
@@ -2,16 +2,14 @@ library(dplyr)
 library(ggplot2)
 library(readxl)
 d <- read.csv("D:/C/ChuyenDe5-R/Dataset/weatherHistory.csv")
-data<-d
-str(data)
 names(d)
 d <- d[,-5]
 d <- d[,-9]
 
 colnames(d)<- c("Date","Summary", "Precip.Type","Temperature","Humidity","Wind.speed","Wind.Bearing.Degree","Visibility","Pressure","Daily.Summary")
+d <- filter(d,d$Precip.Type != 'null')
 view(d)
 
-View(data)
 nameDiagram =c("Temperature", "Humidity", "Wind Speed", "Visibility", "Pressure millibars")
 tablename = c("Temperature", "Humidity", "Wind.speed", "Visibility", "Pressure")
 #danh sach bien phu thuoc
